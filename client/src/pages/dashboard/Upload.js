9c8c4000decb7fabffb9901c3d7a15e4675f7ba9
Gaurav Surtani <g.surtani2309@gmail.com>
2023-09-30 17:00:45 -0700

    Major changes in Upload resources

--- client/src/pages/dashboard/Upload.js
+++ client/src/pages/dashboard/Upload.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './upload.css'
 import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+
 
 
 function ResourceUploadForm() {
@@ -9,6 +14,10 @@ function ResourceUploadForm() {
   const [searchEmail, setSearchEmail] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   
+  let imageUploadBtn = useRef();
+  let pdfUploadBtn = useRef();
+  let videoUploadBtn = useRef();
+  let documentUploadBtn = useRef();
 
   const handleSearchInputChange = (event) => {
     setSearchEmail(event.target.value);
@@ -106,7 +115,6 @@ const handleFileChange = (event, fileType) => {
 
 
   const handleSubmit = async () => {
-
     try {
      
       await fetch('api/upload', {
@@ -146,37 +154,53 @@ const handleFileChange = (event, fileType) => {
     <br/>
     You can also search for your resource with your user ID below and check the details.</p> 
 
-    <div class="row g-3 mb-4">
-    <div class="form-floating col">        
-        <input type="text" class="form-control" id="formGroupResourceTopic" placeholder="Enter Resource Topic" aria-label="Resource Topic" />
-        <label for="formGroupResourceTopic" class="">Resource Topic</label>
-    </div>
-    <div class="form-floating col">        
-        <input type="text" class="form-control" id="formGroupResourceType" placeholder="Enter Resource Type" aria-label="Resource Type" />
-        <label for="formGroupResourceType" class="">Resource Type</label>
-    </div>
-    </div>
-    <div class="form-floating col-12 mb-4">
-        <input type="text" class="form-control" id="formGroupSubject" placeholder="Enter Subject Here"/>
-        <label for="formGroupSubject" class="">Subject</label>
-    </div>
-    <div class="form-floating col-12 mb-4">
-        <textarea class="min-height--100 form-control" id="formGroupContent" placeholder="Enter Content Here" />
-        <label for="formGroupContent" class="form-label">Content</label>
-    </div>
-    <div class="form-floating col-12 mb-4">
-        <input type="text" class="form-control" id="formGroupUser" placeholder="Enter User ID Here"/>
-        <label for="formGroupUser" class="">User ID / Email</label>
-    </div>
-    <div class="mb-3">
-      {/* <label for="formGroupFile" class="form-label">File Upload</label> */}
-      <input class="form-control" type="file" id="formGroupFile" />
-    </div>
-        
+    <div className="row g-3 mb-3">
+        <div className="form-floating col">
+          <input type="text" className="form-control" id="formGroupResourceTopic" placeholder="Enter Resource Topic" aria-label="Resource Topic" name="topic"
+          value={resourceData.topic} onChange={handleInputChange}/>
+          <label htmlFor="formGroupResourceTopic" className="">Resource Topic</label>
+        </div>
+        <div className="form-floating col">
+          <input type="text" className="form-control" id="formGroupResourceType" placeholder="Enter Resource Type" aria-label="Resource Type" name="type" value={resourceData.type}onChange={handleInputChange}/>
+          <label htmlFor="formGroupResourceType" className="">Resource Type</label>
+        </div>
+      </div>
+      <div className="form-floating col-12 mb-3">
+        <input type="text" className="form-control" id="formGroupSubject" placeholder="Enter Subject Here" name="subject" value={resourceData.subject} onChange={handleInputChange}/>
+        <label htmlFor="formGroupSubject" className="">Subject</label>
+      </div>
+      <div className="form-floating col-12 mb-3">
+        <textarea className="min-height--75 form-control" id="formGroupContent" placeholder="Enter Content Here" name="content" value={resourceData.content} onChange={handleInputChange} />
+        <label htmlFor="formGroupContent" className="form-label">Content</label>
+      </div>
+      <div className="form-floating col-12 mb-3">
+        <input type="text" className="form-control" id="formGroupUser" placeholder="Enter User ID Here" name="userid" value={resourceData.userid} onChange={handleInputChange}/>
+        <label htmlFor="formGroupUser" className="">User ID / Email</label>
+      </div>
+
+      <InputGroup size="lg" className='mt-2 mb-3'>
+        <InputGroup.Text id="search-input-group"><i className="bi bi-upload"></i></InputGroup.Text>
+        <Form.Control
+          aria-label="Upload box"
+          aria-describedby="inputGroup-sizing-sm"
+          disabled="true"
+          placeholder='Select File Type to Upload:'
+        />
+        <Button variant="outline-primary" id="upload-image-button" className='z-0' onClick={(e) => imageUploadBtn.current?.click()}><i className="far fa-image"></i> Images</Button>
+        <Button variant="outline-primary" id="upload-pdf-button" className='z-0' onClick={(e) => pdfUploadBtn.current?.click()}><i className="far fa-file-alt"></i> PDF's</Button>
+        <Button variant="outline-primary" id="upload-video-button" className='z-0' onClick={(e) => videoUploadBtn.current?.click()}><i className="far fa-file-video"></i> Videos</Button>
+        <Button variant="outline-primary" id="upload-doc-button" className='z-0' onClick={(e) => documentUploadBtn.current?.click()}><i className="far fa-file"></i> Documents</Button>
+      </InputGroup>
+
+      <div className="form-floating col-12 mb-4">
+        <textarea className="form-control" id="formGroupUrl" placeholder="Enter URL Here" name="url" onChange={(e) => handleFileChange(e, "urls")} />
+        <label htmlFor="formGroupContent" className="form-label">URL's (multiple URL's to separated by commas)</label>
+      </div>        
 
     <div className="form-group">
+      <>
       {/* ... Your other input fields for topic, type, etc. */}
-      <label htmlFor="resourceTopic">Resource Topic</label>
+      {/* <label htmlFor="resourceTopic">Resource Topic</label>
           <i className="fas fa-user"></i>
           <input
             type="text"
@@ -227,68 +251,72 @@ const handleFileChange = (event, fileType) => {
             value={resourceData.userid}
             onChange={handleInputChange}
             required
-          />
+          /> */}
 
-
+      </>
       
       {/* File upload section */}
-      <label htmlFor="files">Upload Files</label>
-      <div className="file-upload-icons">
-  
-        <label htmlFor="imageInput" className="file-type-icon">
-      <i className="far fa-image"></i>
-      <input
-        type="file"
-        id="imageInput"
-        multiple accept=".jpg, .jpeg, .png"
-        onChange={(e) => handleFileChange(e, "images")}
-        style={{ display: 'none' }}
-      />
-      <span>Images</span>
-    </label>
+        {/* <label htmlFor="files">Upload Files</label> */}
+        <div className="file-upload-icons d-none">
 
-    <br />
+          <label htmlFor="imageInput" className="file-type-icon">
+            <i className="far fa-image"></i>
+            <input
+              ref={imageUploadBtn}
+              type="file"
+              id="imageInput"
+              multiple accept=".jpg, .jpeg, .png"
+              onChange={(e) => handleFileChange(e, "images")}
+              style={{ display: 'none' }}
+            />
+            <span>Images</span>
+          </label>
 
-    <label htmlFor="pdfInput" className="file-type-icon">
-      <i className="far fa-file-pdf"></i>
-      <input
-        type="file"
-        id="pdfInput"
-        multiple accept=".pdf"
-        onChange={(e) => handleFileChange(e, "pdfs")}
-        style={{ display: 'none' }}
-      />
-      <span>PDFs</span>
-    </label>
+          <br />
 
-    <br />
+          <label htmlFor="pdfInput" className="file-type-icon">
+            <i className="far fa-file-pdf"></i>
+            <input
+              ref={pdfUploadBtn}
+              type="file"
+              id="pdfInput"
+              multiple accept=".pdf"
+              onChange={(e) => handleFileChange(e, "pdfs")}
+              style={{ display: 'none' }}
+            />
+            <span>PDFs</span>
+          </label>
 
-    <label htmlFor="videoInput" className="file-type-icon">
-      <i className="far fa-file-video"></i>
-      <input
-        type="file"
-        id="videoInput"
-        multiple accept=".mp4"
-        onChange={(e) => handleFileChange(e, "videos")}
-        style={{ display: 'none' }}
-      />
-      <span>Videos</span>
-    </label>
+          <br />
 
-    <br />
+          <label htmlFor="videoInput" className="file-type-icon">
+            <i className="far fa-file-video"></i>
+            <input
+              ref={videoUploadBtn}
+              type="file"
+              id="videoInput"
+              multiple accept=".mp4"
+              onChange={(e) => handleFileChange(e, "videos")}
+              style={{ display: 'none' }}
+            />
+            <span>Videos</span>
+          </label>
 
-    <label htmlFor="docInput" className="file-type-icon">
-      <i className="far fa-file-alt"></i>
-      <input
-        type="file"
-        id="docInput"
-       multiple accept=".doc, .docx"
-        onChange={(e) => handleFileChange(e, "documents")}
-        style={{ display: 'none' }}
-      />
-      <span>Documents</span>
-    </label>
-    
+          <br />
+
+          <label htmlFor="docInput" className="file-type-icon">
+            <i className="far fa-file-alt"></i>
+            <input
+              ref={documentUploadBtn}
+              type="file"
+              id="docInput"
+              multiple accept=".doc, .docx"
+              onChange={(e) => handleFileChange(e, "documents")}
+              style={{ display: 'none' }}
+            />
+            <span>Documents</span>
+          </label>
+
     <br />
 
     <label htmlFor="urlInput" className="file-type-icon">
@@ -305,20 +333,18 @@ const handleFileChange = (event, fileType) => {
 
       </div>
       <div className="uploaded-files">
-        <h4>Uploaded Files:</h4>
+        { uploadedFiles.length != 0 && <><h4>Uploaded Files:</h4></>}
         <ul>
           {uploadedFiles.map((file, index) => (
             <li key={index}>{`${file.type}: ${file.name}`}</li>
           ))}
         </ul>
       </div>
-
-      <button onClick={handleSubmit}>Publish</button>
-
-      <br />
-      <br />
-
-      <button onClick={handleClear}>Clear</button>
+      <Stack className='mt-4' direction="horizontal" gap={3}>
+        <Button size="lg" variant="primary ms-auto" onClick={handleSubmit}>Publish</Button>
+        <Button size="lg" variant="primary " onClick={handleClear}>Clear</Button>
+      </Stack>
+      
       {message && <p>{message}</p>}
        
        <br />
